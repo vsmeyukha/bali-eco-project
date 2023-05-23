3040f196c000013dc9b70877e78a8953bb14426b
vsmeyukha <vsmeyukha@gmail.com>
2023-05-23 21:30:56 +0300

    блок о нас сделан для всех разрешений

--- src/components/MainPageLoggedIn/AboutUs.tsx
+++ src/components/MainPageLoggedIn/AboutUs.tsx
@@ -9,23 +9,45 @@ const AboutUs: React.FC = (): ReactElement => {
   arr.pop();
 
   return (
-    <div className="flex flex-col items-center 1440px:mx-[152px] xl:mx-[100px] lg:mx-[50px] pt-[30px] mb-[96px]">
+    <div className="
+    flex
+    flex-col
+    items-center
+    1440px:mx-[152px]
+    xl:mx-[100px]
+    lg:mx-[50px]
+    md:mx-[40px]
+    sm:mx-[32px]
+    mx-[20px]
+    pt-[30px]
+    mb-[96px]"
+    >
       <div className="flex flex-col items-start">
         <h2
           className="
             text-[#00265F]
-            text-[32px]
-            leading-[38px]
+            lg:text-[32px]
+            lg:leading-[38px]
+            md:text-[28px]
+            md:leading-[38px]
+            sm:text-[22px]
+            sm:leading-[26px]
+            text-[18px]
+            leading-[21px]
             font-oceanic-bold
             "
         >О нас</h2>
-        <div className="flex xl:flex-row lg:flex-col-reverse justify-center mt-[32px]">
+        <div className="flex xl:flex-row flex-col-reverse justify-center mt-[32px]">
           <div
             className="
               grid
-              grid-cols-2
-              grid-rows-2
-              gap-[30px]
+              sm:grid-cols-2
+              sm:grid-rows-2
+              grid-rows-4
+              grid-cols-1
+              md:gap-[30px]
+              sm:gap-[24px]
+              gap-[16px]
               font-montserrat
               text-[18px]
               leading-[26px]
@@ -35,7 +57,7 @@ const AboutUs: React.FC = (): ReactElement => {
             {
               arr.map((paragraph, index) => {
                 return (
-                  <div key={index} className="max-w-[315px]">
+                  <div key={index} className="sm:max-w-[315px]">
                     <div className="w-[32px] h-[32px] bg-gray-300 rounded-[8px] flex justify-center items-center">
                       <PlanetIcon />
                     </div>
@@ -46,9 +68,24 @@ const AboutUs: React.FC = (): ReactElement => {
               })
             }
           </div>
-          <div className="1440px:min-w-[560px] xl:min-w-[500px] lg:min-w-[450px] lg:min-h-[500px] ml-[30px] relative mb-[36px]">
+          {/* <div className="1440px:min-w-[560px] xl:min-w-[500px] lg:min-w-[450px] ml-[30px] relative mb-[36px]">
             <Image src={JellyFish} alt="jellyfish" fill className="object-cover object-center rounded-[10px]"/>
-          </div>
+          </div> */}
+          <Image
+            src={JellyFish}
+            alt="jellyfish"
+            className="
+            lg:min-w-[450px]
+            object-cover
+            object-center
+            rounded-[10px]
+            xl:ml-[30px]
+            xl:mb-0
+            md:mb-[32px]
+            sm:mb-[24px]
+            mb-[16px]
+            "
+          />
         </div>
       </div>
     </div>
